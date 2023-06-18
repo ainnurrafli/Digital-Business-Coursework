288b853967751ed0a5902d2191c07676a98ad0ce
ema-devani <vaniputri0603@gmail.com>
2023-06-18 21:09:35 +0700

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,43 @@ module.exports = {
         'gilroy-light': ['Gilroy-Light', 'sans-serif'],
         'gilroy-medium': ['Gilroy-Medium','sans-serif'],
         'gilroy-reguler' : ['Gilroy-Regular', 'sans-serif'],
-        'hind-Jalandhar': ['Hind Jalandhar', 'sans-serif']
-                                                
+        'hind-Jalandhar': ['Hind Jalandhar', 'sans-serif'],
+        'poppins' : ['poppins'], 
+        'gilda-display' : ['Gilda Display']                                  
       },
       colors: {
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"},
-        warna1 : {"warna1":"#FFAF6E"}
+        warna1 : {"warna1":"#FFAF6E"},
+        warna2 : {"warna2":"#FDF9E5"},
+        warna3 : {"warna3":"#FFAF6E"}
+      },
+      
+      margin:{
+        '5000px' : '1200px',
+        '1000px' : '1000px',
+        '493px' : '493px',
+        '180px' :  '180px'
+      },
+      width:{
+        '1081px' : '1081px',
+        '350px' : '350px'
+      },
+      height:{
+        '1287px' :  '1287px',
+        '350px' : '350px',
+        '800px' : '800px',
+        '500px' : '500px'
+      },
+      minHeight:{
+        '800px' : '800px'
+      },
+      minWidth:{
+        '900px' : '900px'
+      },
+      backdropBlur:{
+        '200px' : '200px'
       }
+
     },
   },
   plugins: [require('flowbite/plugin')],
